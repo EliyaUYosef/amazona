bdfbb1bdd89dd5ebc1026053682acf1adc18619b
Eliya.Y <Eliya.Yosef@gmail.com>
2022-12-14 16:57:36 +0200

    40 - Create-Edit-Product-Screen

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -30,6 +30,7 @@ import ProtectedRoute from "./components/ProtectedRoute";
 import DashboardScreen from "./screens/DashboardScreen";
 import AdminRoute from "./components/AdminRoute";
 import ProductListScreen from "./screens/ProductListScreen";
+import ProductEditScreen from "./screens/ProductEditScreen";
 
 function App() {
   const { state, dispatch: ctxDispatch } = useContext(Store);
@@ -180,6 +181,14 @@ function App() {
                 element={
                   <AdminRoute>
                     <DashboardScreen />
+                  </AdminRoute>
+                }
+              ></Route>
+              <Route
+                path="/admin/product/:id"
+                element={
+                  <AdminRoute>
+                    <ProductEditScreen />
                   </AdminRoute>
                 }
               ></Route>
